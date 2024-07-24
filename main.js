9a9cd9bc7e4e56aa95cfe2afe767421c2931aeb2
Yumna <yumnamubeen0019@gmail.com>
2024-07-24 22:42:17 +0500

    Initial

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let resetBtn = document.querySelector(".reset-btn");
 let newBtn = document.querySelector(".new-btn");
 let msgBox = document.querySelector(".msg-box");
 let msg = document.querySelector(".msg");
+let turnInfo = document.querySelector("#turn-info");
 
 let turnO = true;
 const win_patterns = [[0,1,2],
@@ -12,7 +13,13 @@ const resetGame = () => {
     turnO = true;
     enableBoxes();
     msgBox.classList.add("hide");
+    updateTurnInfo();
 };
+
+const updateTurnInfo = () => {
+    turnInfo.innerText = turnO ? "It's O's turn" : "It's X's turn";
+};
+
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
@@ -25,7 +32,7 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
-
+        updateTurnInfo();
         checkWinner();
     });
 });
@@ -49,19 +56,41 @@ const showWinner = (winner) => {
     disableBoxes();
 }
 
+
+const showDraw = () => {
+    msg.innerText = 'Draw Game';
+    msgBox.classList.remove("hide");
+    disableBoxes();
+};
+
 const checkWinner = () => {
+    let allBoxesFilled = true;
+
     for (let pattern of win_patterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
-    
-        if (pos1Val!="" && pos2Val!="" && pos3Val!="") {
-            if (pos1Val===pos2Val && pos2Val===pos3Val) {
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
+                return;
             }
         }
+    }
+
+    for (let box of boxes) {
+        if (box.innerText === "") {
+            allBoxesFilled = false;
+            break;
+        }
+    }
+
+    if (allBoxesFilled) {
+        showDraw();
     }
 };
 
 newBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
+updateTurnInfo();
