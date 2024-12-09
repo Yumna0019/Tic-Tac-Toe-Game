5c82d40619a40bbafb28b071a125d913c917db06
Yumna <yumnamubeen0019@gmail.com>
2024-12-09 21:22:48 +0500

    Tic tac toe

--- main.js
+++ main.js
@@ -4,62 +4,80 @@ let newBtn = document.querySelector(".new-btn");
 let msgBox = document.querySelector(".msg-box");
 let msg = document.querySelector(".msg");
 let turnInfo = document.querySelector("#turn-info");
+let modeSelect = document.querySelector("#mode");
 
 let turnO = true;
-const win_patterns = [[0,1,2],
-[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
+let isAIMode = false;
+let gameEnded = false;
+let lastWinningPattern = [];  
+
+const win_patterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 
 const resetGame = () => {
     turnO = true;
+    gameEnded = false;
+    isAIMode = modeSelect.value === "ai";
+    lastWinningPattern = []; 
     enableBoxes();
     msgBox.classList.add("hide");
     updateTurnInfo();
 };
 
 const updateTurnInfo = () => {
-    turnInfo.innerText = turnO ? "It's O's turn" : "It's X's turn";
+    if (!gameEnded) {
+        if (turnO) {
+            turnInfo.innerText = "It's your turn (O)";
+        } else if (isAIMode) {
+            turnInfo.innerText = "AI is thinking...";
+        } else {
+            turnInfo.innerText = "It's your friend's turn (X)";
+        }
+    }
 };
 
-
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if(turnO) {
-            box.innerText = "O";
-            turnO = false;
-        }
-        else {
-            box.innerText = "X";
-            turnO = true;
-        }
-        box.disabled = true;
-        updateTurnInfo();
-        checkWinner();
-    });
-});
-
 const disableBoxes = () => {
-    for(box of boxes) {
+    for (let box of boxes) {
         box.disabled = true;
     }
 };
 
 const enableBoxes = () => {
-    for(let box of boxes) {
-        box.disabled =  false;
+    for (let box of boxes) {
+        box.disabled = false;
         box.innerText = "";
+        box.classList.remove("winning-box");
     }
-}
+};
 
-const showWinner = (winner) => {
-    msg.innerText = 'Congratulations, Winner is '+ winner;
-    msgBox.classList.remove("hide");
-    disableBoxes();
-}
+const highlightWinningBoxes = (pattern) => {
+    pattern.forEach(index => {
+        boxes[index].classList.add("winning-box"); 
+    });
+};
 
+const showWinner = (winner, pattern) => {
+    highlightWinningBoxes(pattern); 
+    setTimeout(() => {
+        msg.innerText = `Congratulations, Winner is ${winner}`;
+        msgBox.classList.remove("hide");
+        gameEnded = true;
+        disableBoxes();
+    }, 500); 
+};
 
 const showDraw = () => {
-    msg.innerText = 'Draw Game';
+    msg.innerText = "Draw Game";
     msgBox.classList.remove("hide");
+    gameEnded = true;
     disableBoxes();
 };
 
@@ -71,10 +89,11 @@ const checkWinner = () => {
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
-                showWinner(pos1Val);
-                return;
+                lastWinningPattern = pattern; 
+                showWinner(pos1Val, pattern); 
+                return true; 
             }
         }
     }
@@ -89,8 +108,60 @@ const checkWinner = () => {
     if (allBoxesFilled) {
         showDraw();
     }
+
+    return false;
 };
+
+const makeAIMove = () => {
+    if (gameEnded) return; 
+    turnInfo.innerText = "AI is thinking...";
+
+    const emptyBoxes = [...boxes].filter(box => box.innerText === "");
+
+    for (let box of emptyBoxes) {
+        box.innerText = "X";
+
+        if (checkWinner()) {
+            box.disabled = true; 
+            return;
+        }
+        box.innerText = "";
+    }
+
+    if (!gameEnded && emptyBoxes.length > 0) {
+        const randomIndex = Math.floor(Math.random() * emptyBoxes.length);
+        const chosenBox = emptyBoxes[randomIndex];
+        chosenBox.innerText = "X";
+        chosenBox.disabled = true;
+        turnO = true;
+        updateTurnInfo();
+        checkWinner();
+    }
+};
+
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        if (gameEnded) return;
+
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        updateTurnInfo();
+        checkWinner();
+
+        if (!turnO && isAIMode && !gameEnded) {
+            setTimeout(makeAIMove, 500);
+        }
+    });
+});
 
 newBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
+modeSelect.addEventListener("change", resetGame);
+
 updateTurnInfo();
